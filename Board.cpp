ce4fae09e34f8d756873f41361c3025214fecb5b
ammily00 <67396336+ammily00@users.noreply.github.com>
2020-06-30 15:07:10 -0700

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -15,8 +15,36 @@ Board::~Board(){
 void Board::printBoard(){
     for (int i = 0; i < size; i++){
         for (int j = 0; j < size; j++){
-            cout << grid[i][j] << endl;
+            //print the index number for rows
+            if (j == 0)
+                cout << i+1 << " ";
+
+            //print the stones for different situations
+            switch(grid[i][j]){
+                case stoneBlack:
+                    cout << "x ";
+                    break;
+                case stoneWhite:
+                    cout << "o ";
+                    break;
+                default:
+                    cout << "  ";
+                    break;
+            }
+
+            //print the newline when the current line ends
+            if (j == size - 1)
+                cout << endl;
         }
+    }
+
+    //print the index number for columns
+    for (char k = 'A'; k < 'A' + size; k++){
+        if (k == 'A')
+            cout << "  ";
+        cout << k << " ";
+        if (k == 'A' + size - 1)
+            cout << endl;
     }
 }
 
@@ -29,29 +57,21 @@ Stone * Board::getStone(int x, int y){
             stone.y = y;
             stone.color = grid[x][y];
             return stone;
- * }
- */
+    }
 }
 
 void Board::placeStone(int x, int y, stoneColor color){
-/*
- * grid[x][y] = color;
- */
+    grid[x][y] = color;
 }
 
 void Board::removeStone(int x, int y){
-/*
- * grid[x][y] = NOSTONE;
- */
+    grid[x][y] = NOSTONE;
 }
 
 void Board::resetBoard(){
-/*
- * In grid[size][size]
- *      for i in size
- *          for j in size
- *              grid[i][j] = NOSTONE;
- *          end
- *      end
- */
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            grid[i][j] == NOSTONE;
+        }
+    }
 }
